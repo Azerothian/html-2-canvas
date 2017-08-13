edd5387fee55885619a5f858820ff870f8b04454
Matthew Mckenzie <mmckenzie@vostronet.com>
2017-08-14 09:55:59 +1000

    started processing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import parseCss from "css-parse";
 import CSSselect from "css-select";
 
 
+function parsePx(px) {
+  if(!Number.isInteger(px)) {
+    return Number(px.replace(/px$/, ''));
+  }
+  return px;
+}
+
 function applyStylesheet(stylesheet, dom) {
   // console.log("defaultStylesheet", stylesheet.rules);
   stylesheet.rules.forEach((rule) => {
@@ -40,6 +47,115 @@ function applyStyleTag(node) {
 }
 
 
+function extractShorthandSpacing(s) {
+  const e = s.split(" ").map((r) => parsePx(r));
+  if (e.length === 1) {
+    return {
+      top: e[0],
+      bottom: e[0],
+      left: e[0],
+      right: e[0],
+    };
+  }
+  if (e.length === 2) {
+    return {
+      top: e[0],
+      bottom: e[0],
+      left: e[1],
+      right: e[1],
+    };
+  }
+  if (e.length === 3) {
+    return {
+      top: e[0],
+      bottom: e[2],
+      left: e[1],
+      right: e[1],
+    };
+  }
+  if (e.length === 4) {
+    return {
+      top: e[0],
+      bottom: e[2],
+      left: e[3],
+      right: e[1],
+    };
+  }
+  return {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  };
+}
+
+/* 
+-> margin-top -> border -> padding-top -> contents 
+-> padding-bottom -> border -> margin-bottom
+*/
+
+const inheritable = [
+  "font",
+  "text"
+];
+
+async function processElement(element, parentCtx, cx2d, canvas) {
+  const ctx = {
+    render: {
+      x: parentCtx.render.x || 0,
+      y: parentCtx.render.y || 0,
+    },
+    style: {
+      padding: {top: 0, bottom: 0, left: 0, right: 0},
+      margin: {top: 0, bottom: 0, left: 0, right: 0},
+    },
+  };
+  if (parentCtx.style) {
+    ctx.style = Object.keys(parentCtx.style).reduce((s, key) => {
+      for (let x in inheritable) {
+        if (key.indexOf(inheritable[x]) > -1) {
+          s[key] = parentCtx.style[key];
+        }
+      }
+    }, ctx.style);
+  }
+  ctx.style = Object.keys(element.format).reduce((s, key) => {
+    const value = element.format[key];
+    if (key.indexOf("margin-") > -1 || key.indexOf("padding-") > -1) {
+      const k = key.split("-");
+      s[k[0]][k[1]] = parsePx(value);
+    } else if (key === "margin" || key === "padding") {
+      s[key] = extractShorthandSpacing(value);
+    } else {
+      s[key] = value;
+    }
+    return s;
+  }, );
+
+
+  
+  ctx.render.x += ctx.style.margin.top;
+  ctx.render.y += ctx.style.margin.left;
+
+  ctx.render.x += ctx.style.padding.top;
+  ctx.render.y += ctx.style.padding.left;
+
+  ctx.outerWidth = context.innerWidth - ctx.style.margin.left - ctx.style.margin.right;
+  ctx.innerWidth = ctx.outerWidth - ctx.style.padding.left - ctx.style.padding.right;
+
+  
+
+  if (element.children) {
+    return element.children.reduce((promise, child) => {
+      // console.log("processing children", child);
+      return promise.then((context) => {
+        return processElement(child, ctx, cx2d, canvas);
+      });
+    }, Promise.resolve(context));
+  }
+  return undefined;
+}
+
 export default async function html2canvas(html, canvas, options = {}) {
   let {stylesheet} = (options.stylesheet) ? parseCss(options.stylesheet) : {};
   const dom = parse5.parse(html, {
@@ -49,10 +165,10 @@ export default async function html2canvas(html, canvas, options = {}) {
     applyStylesheet(stylesheet, dom);
   }
   applyStyleTag(dom);
-
-
-  // console.log("dom", dom);
-  const results = CSSselect.selectAll("p.test", dom);
-  console.log("p", results);
+  const cx2d = canvas.getContext("2d");
+  await processElement(dom, {
+    innerWidth: canvas.width,
+  }, cx2d, canvas);
 
 }
+
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -2,6 +2,8 @@ import html2canvas from "../index";
 import Canvas from "canvas";
 
 const stylesheet = `body {
+  font-family: Arial;
+  font-size: 12px;
   padding: 20px;
 }
 div > a:first-child {
@@ -14,10 +16,19 @@ div.row > p:first-child {
   padding-top: 10px;
 }`;
 
+
 describe("base tests", () => {
   it("initial test", () => {
+    const html = `<html>
+<body>
+  <div class="row">
+    <p>Howdy</p>
+    <p class="test" style="padding-top: 20px">2nd</p>
+  </div
+</body>
+</html>`;
     const canvas = new Canvas(200, 200);
-    html2canvas("<html><body><div class=\"row\"><p>Howdy</p><p class=\"test\" style=\"padding-top: 20px\">2nd</p></div</body></html>", canvas, {
+    html2canvas(html, canvas, {
       stylesheet,
     });
   });
